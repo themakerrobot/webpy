44cce77888e2e361b3289303e686dda1e4f1a1b2
Yunjai, Lee <leeyunjai@circul.us>
2024-06-04 12:37:45 +0900

    Update index.js

--- static/index.js
+++ static/index.js
@@ -8,42 +8,42 @@ window.onload = function() {
         theme: "monokai"
     });
     editor.getWrapperElement().style.fontSize = "20px";
-};
 
-function changeFontSize() {
-    var fontSize = document.getElementById("font-size").value + "px";
-    editor.getWrapperElement().style.fontSize = fontSize;
-    editor.refresh();
-}
-
-document.getElementById("run").addEventListener("click", ()=> {
-    Sk.execLimit = 60 * 1000;
-    var code = editor.getValue();
-    document.getElementById("output-content").innerText = "";  // 이전 출력 내용 초기화
-
-    Sk.configure({
-        output: function (text) {
-            document.getElementById("output-content").innerText += text.replace(/^\n+/, ''); // 공백 제거
-        },
-        read: function (filename) {
-            if (Sk.builtinFiles === undefined || Sk.builtinFiles["files"][filename] === undefined) {
-                throw "File not found: '" + filename + "'";
+    function changeFontSize() {
+        var fontSize = document.getElementById("font-size").value + "px";
+        editor.getWrapperElement().style.fontSize = fontSize;
+        editor.refresh();
+    }
+    
+    document.getElementById("run").addEventListener("click", ()=> {
+        Sk.execLimit = 60 * 1000;
+        var code = editor.getValue();
+        document.getElementById("output-content").innerText = "";  // 이전 출력 내용 초기화
+    
+        Sk.configure({
+            output: function (text) {
+                document.getElementById("output-content").innerText += text.replace(/^\n+/, ''); // 공백 제거
+            },
+            read: function (filename) {
+                if (Sk.builtinFiles === undefined || Sk.builtinFiles["files"][filename] === undefined) {
+                    throw "File not found: '" + filename + "'";
+                }
+                return Sk.builtinFiles["files"][filename];
             }
-            return Sk.builtinFiles["files"][filename];
-        }
+        });
+    
+        (Sk.TurtleGraphics || (Sk.TurtleGraphics = {})).target = 'turtle';
+    
+        Sk.misceval.asyncToPromise(function () {
+            return Sk.importMainWithBody("<stdin>", false, code, true);
+        }).then(function (mod) {
+            document.getElementById("output-content").innerText += '\n\nOk';
+        }).catch(function (err) {
+            document.getElementById("output-content").innerText = err.toString();
+        });
     });
-
-    (Sk.TurtleGraphics || (Sk.TurtleGraphics = {})).target = 'turtle';
-
-    Sk.misceval.asyncToPromise(function () {
-        return Sk.importMainWithBody("<stdin>", false, code, true);
-    }).then(function (mod) {
-        document.getElementById("output-content").innerText += '\n\nOk';
-    }).catch(function (err) {
-        document.getElementById("output-content").innerText = err.toString();
+    
+    document.getElementById("stop").addEventListener("click", ()=> {
+        Sk.execLimit = 0; // 코드 실행 중지
     });
-});
-
-document.getElementById("stop").addEventListener("click", ()=> {
-    Sk.execLimit = 0; // 코드 실행 중지
-});
+};
